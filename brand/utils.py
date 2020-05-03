12c208a50bf7a69a6aa1ab5813631406ddc6b3f7
nachtsky <nachtsky617@163.com>
2020-05-03 14:27:39 +0800

    center brand embeddings before project onto subspace

--- brand/utils.py
+++ brand/utils.py
@@ -1,7 +1,18 @@
 import numpy as np
 
-def get_embedding_mat(words, embedding):
+def get_embedding_mat(words, embedding, mean=False):
     mat = []
     for word in words:
         mat.append(embedding[word])
-    return np.asarray(mat)
+    mat = np.asarray(mat)
+    if not mean:
+        return mat
+    else:
+        return mat, np.mean(mat, axis=0)
+
+def get_word_vector(word, kv, mean=None):
+    word_vec = kv[word]
+    if mean is not None:
+        return word_vec-mean
+    else:
+        return word_vec
